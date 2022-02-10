b3889fc75190dc8a59bc7c22d1b8ff2da417e59a
Rafael Khachikian <Khachikyan38@gmail.com>
2022-02-10 20:53:49 +0300

    radio buttons

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
+const modalCloseBtn = document.querySelector(".js-modal-close");
+const continueBtn = document.querySelectorAll(".js-continue");
+const bookmark = document.querySelector(".js-bookmark");
+const modal = document.querySelector(".modal__wrapper");
+const radioBtn = document.querySelectorAll(".js-radio");
+const select = document.querySelectorAll(".js-select");
+const pledge = document.querySelectorAll(".js-pledge");
 const burger = document.querySelector(".js-burger");
 const back = document.querySelector(".js-back");
-const bookmark = document.querySelector(".js-bookmark");
-const select = document.querySelectorAll(".js-select");
-const modalClose = document.querySelector(".js-modal-close");
-const continueBtn = document.querySelectorAll(".js-continue");
 const nav = document.querySelector(".js-nav");
-const modal = document.querySelector(".modal__wrapper");
-const modalBtn = [back, ...select];
+const borderGreen = document.querySelectorAll(".border");
+const input = document.querySelectorAll(".js-input");
+
+const modalOpenBtn = [back, ...select];
 
 function navigation() {
   if (!nav.classList.contains("navigation__active")) {
@@ -22,6 +27,13 @@ function openModal() {
   modal.classList.add("modal__active");
 }
 
+function closeModal() {
+  modal.classList.remove("modal__active");
+  radioBtn.forEach((radio) => (radio.checked = false));
+  borderGreen.forEach((box) => box.classList.remove("box__active"));
+  pledge.forEach((el) => el.classList.remove("pledge__active"));
+}
+
 function bookmarkBtn() {
   if (!bookmark.classList.contains("bookmarked")) {
     bookmark.classList.add("bookmarked");
@@ -32,6 +44,27 @@ function bookmarkBtn() {
   }
 }
 
+function pledgeActive(e) {
+  const close = e.target.parentElement.parentElement.nextElementSibling;
+  const box = e.target.parentElement.parentElement.parentElement;
+
+  borderGreen.forEach((box) => box.classList.remove("box__active"));
+
+  if (box.classList.contains("border")) {
+    box.classList.add("box__active");
+  }
+
+  pledge.forEach((el) => el.classList.remove("pledge__active"));
+
+  if (!close.classList.contains("pledge__active")) {
+    close.classList.add("pledge__active");
+  }
+}
+
+function checkInput() {}
+
 burger.addEventListener("click", navigation);
-modalBtn.forEach((btn) => btn.addEventListener("click", openModal));
 bookmark.addEventListener("click", bookmarkBtn);
+modalOpenBtn.forEach((btn) => btn.addEventListener("click", openModal));
+modalCloseBtn.addEventListener("click", closeModal);
+radioBtn.forEach((radio) => radio.addEventListener("click", pledgeActive));
